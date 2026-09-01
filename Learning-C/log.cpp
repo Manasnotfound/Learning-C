42a62936d654b05f6657392464434b951950ea6d
Manas Mishra <manas@Manass-MacBook-Air.local>
2026-09-01 16:38:54 +0530

    While and do While

--- Learning-C/log.cpp
+++ Learning-C/log.cpp
@@ -9,9 +9,31 @@ int main()
 	for (; LOOP;)
 	{cout << "loop Works" << endl;
 		n++;
-		if (n > 20)
+		if (n > 4)
 		{
 			LOOP = false;
 		}
 	}	
+	cout << "=========================" << endl;
+	n = 1;
+	while (!LOOP)
+	{
+		cout << "while Works" << endl;
+		n++;
+		if (n > 4)
+		{
+			LOOP = true;	
+		}
+	}
+	cout << "========================="<< n << endl;
+	do
+	{
+		cout << "do while Works" << endl;
+		n++;
+		if (n > 8)
+		{
+			LOOP = false;
+		}
+	} while (!LOOP);
+	
 }
