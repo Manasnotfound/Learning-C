0b390f88861fa569aed21eb6ad62a02c55b58e57
Manas Mishra <manas@Manass-MacBook-Air.local>
2026-09-01 18:00:39 +0530

    Pointers (Memory address)

--- Learning-C/log.cpp
+++ Learning-C/log.cpp
@@ -4,10 +4,9 @@ using namespace std;
 
 int main()
 {
-	for (int i = 0; i < 10; i++)
-	{
-		if (i % 2 ==0)
-		    return 0;
-		cout << i << endl;
-	}
+	int var = 10;
+	int* ptr = &var;
+	cout << "Value of var: " << var << endl;
+	cout << "Address of var: " << ptr << endl;
+	
 }
