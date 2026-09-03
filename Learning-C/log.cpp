f1fd1fd0ba51d08f9c88923976888ce1a9eb9a53
Manas Mishra <manasmishra94798@gmail.com>
2026-09-03 15:45:36 +0530

    IsPalindrome

--- Learning-C/log.cpp
+++ Learning-C/log.cpp
@@ -1,22 +1,33 @@
 #include <iostream>
 #include<stdlib.h>
 using namespace std;
-#include "log.h"
 
-void increment(int& value)
+bool isPalindrome(int x) 
 {
-	value++;
+        string d = to_string(x);
+		int len = d.length();
+		for(int i=0;i<len;i++)
+		{
+			if(i>=len/2) break;
+			if(d[i]!=d[len-i-1])return false;
+		}
+		return true;
 }
 
-int main()
-{
-	int a = 10;
-	int b = 90;
+int main(){
+	int x; cin>> x;
+	cout<<isPalindrome(x);
 	
-	int& ref = a;
-	ref = b;
-	
-	increment(a);
-	log(a);
-
 }
+
+
+
+//  void increment(int& value){
+//         value++;
+//     }
+//     bool isPalindrome(int x) {
+//         string d = to_string(x);
+//         int i = -1;
+//         increment(i);
+//         return d(i)
+        
