a5b84a7aeb366bd62f5fa3cf4b6fc9cbdf00b945
Manas Mishra <manasmishra94798@gmail.com>
2026-09-04 05:38:15 +0530

    Static2

--- Learning-C/Math.cpp
+++ Learning-C/Math.cpp
@@ -2,6 +2,11 @@
 
 extern int s_variable;
 
+static void increment(int& value)
+{
+	value++;
+}
+
 int main()
 {
 	std::cout << "Static variable value: " << s_variable << std::endl;
---
+++ Learning-C/Static.cpp
@@ -0,0 +1,6 @@
+int s_variable = 28;
+
+static void increment(int& value)
+{
+    value++;
+}
