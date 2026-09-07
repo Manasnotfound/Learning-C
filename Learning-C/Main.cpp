32dd131f9cfe2281de5ec3165c489f9cdf9ab020
Manas Mishra <manasmishra94798@gmail.com>
2026-09-08 03:37:28 +0530

    Enum

--- Learning-C/Main.cpp
+++ Learning-C/Main.cpp
@@ -1,32 +1,15 @@
 #include <iostream>
 
-struct Entity
+enum Example : unsigned int
 {
-	static int x, y;
-
-	static void Print()
-	{
-		std::cout << x << ", " << y << std::endl;
-	}
+	A = 8, B, C
 };
 
-int Entity::x;
-int Entity::y;
+
 
 int main()
 {
-	Entity e;
-	Entity::x = 5;
-	Entity::y = 10;
-
-	Entity e1;
-	Entity::x = 15;
-	Entity::y = 20;
-
-
-	Entity::Print();
-	Entity::Print();
-
-	return 0;
+	Example value = B;
+	std::cout << "Value: " << value << std::endl;
+	std::cin.get();
 }
-
