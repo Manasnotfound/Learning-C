e1187c0db65d3633a62e95230ec52d4947ee1f03
Manas Mishra <manasmishra94798@gmail.com>
2026-09-07 22:14:44 +0530

    static and classes

--- Learning-C/Main.cpp
+++ Learning-C/Main.cpp
@@ -1,23 +1,32 @@
 #include <iostream>
-#include "Log.h"
 
-int MultiplyAndLog(int a, int b)
+struct Entity
 {
-	int result = a * b;
-	log("The result of multiplying 5 and 3 is: 15");
-	return result;
-}
+    int x, y;
 
-void Log(const char* message)
-{
-	std::cout << message << std::endl;
-}
-
+    void Print()
+    {
+        std::cout << x << ", " << y << std::endl;
+    }
+};
 
 int main()
 {
-	Log("Starting the program...");
-	MultiplyAndLog(5, 3);
-	MultiplyAndLog(10, 2);
-	MultiplyAndLog(7, 4);
+    Entity e;
+    e.x = 5;
+    e.y = 10;
+
+    Entity e1;
+    e1.x = 15;
+    e1.y = 20;
+
+    Entity e2;
+    e2.x = 25;
+    e2.y = 30;
+
+    e.Print();
+    e1.Print();
+    e2.Print();
+
+    return 0;
 }
