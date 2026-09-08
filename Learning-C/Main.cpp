8110e5c5f25c9eeab1302268c6244b687f4f7b12
Manas Mishra <manasmishra94798@gmail.com>
2026-09-08 18:16:19 +0530

    constructor with init

--- Learning-C/Main.cpp
+++ Learning-C/Main.cpp
@@ -5,6 +5,12 @@ class Entity
 public:
 	float X, Y;
 
+	void Init()
+	{
+		X = 0.0f;
+		Y = 0.0f;
+	}
+
 	void print()
 	{
 		std::cout << X << ", " << Y << std::endl;
@@ -14,5 +20,7 @@ public:
 int main()
 {
 	Entity e;
+	e.Init();
+	std::cout << e.X << ", " << e.Y << std::endl;
 	e.print();
 }
