1afaf38bee8d412c186cb147db557f851b90fe90
Manas Mishra <manasmishra94798@gmail.com>
2026-09-18 18:54:41 +0530

    Visibility Public

---
+++ Learning-C/Visibility.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+class Entity{
+    public:
+        float X, Y;
+        void Print() {}
+    public:
+        Entity()
+        {
+            X = 0;
+            Print();
+        }
+};
+
+class Public : public Entity
+{
+    public:
+        Public(){
+            X = 2.0;
+            Print();
+        }
+};
+
+int main(){
+    Public player;
+    player.X = 0.2;
+    player.Y = 0.8;
+    player.Print();
+}
