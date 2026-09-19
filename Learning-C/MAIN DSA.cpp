f77c9533e8d8b36e88bd2ad2a9b983248833453d
Manas Mishra <manasmishra94798@gmail.com>
2026-09-19 23:06:08 +0530

    Vector

--- Learning-C/MAIN DSA.cpp
+++ Learning-C/MAIN DSA.cpp
@@ -16,21 +16,33 @@ using namespace std;
 //}
 
 void Vector(){
-    vector<int> v;
-    v.push_back(1); //{1}
-    v.emplace_back(2); //{1, 2}
+    // vector<int> v;
+    // v.push_back(1); //{1}
+    // v.emplace_back(2); //{1, 2}
 
-    vector<pair<int, int>>vec;
+    // vector<pair<int, int>>vec;
 
-    v.push_back({1, 4});
-    v.emplace_back({1, 4});
+    // v.push_back({1, 4});
+    // v.emplace_back({1, 4});
 
-    vector<int> v(5, 100);
+    // vector<int> v(5, 100);
 
-    vector<int> v(5);
+    // vector<int> v(5);
 
-    vector<int> v1(6, 90);
+    vector<int> v1({6, 0, 5, 67});
     vector<int> v2(v1);
+
+    vector<int>::iterator it = v1.begin();
+    it++;
+    cout << *(it) <<" ";
+    it + 3;
+    cout << *(it) <<"\n";
+
+    cout << v1.back() << "  this is the end";
+
+    // vector<int>::iterator it = v1.end();
+    // vector<int>::iterator it = v1.rend();
+    // vector<int>::iterator it = v1.rbegin();
 }
 
 int main(){
