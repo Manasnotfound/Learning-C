ce2ea5f247325cdc9826a1e47dd60ae339c350d4
Manas Mishra <manasmishra94798@gmail.com>
2026-09-19 16:49:15 +0530

    vector

--- Learning-C/MAIN DSA.cpp
+++ Learning-C/MAIN DSA.cpp
@@ -17,7 +17,20 @@ using namespace std;
 
 void Vector(){
     vector<int> v;
-    v.push_back(1);
+    v.push_back(1); //{1}
+    v.emplace_back(2); //{1, 2}
+
+    vector<pair<int, int>>vec;
+
+    v.push_back({1, 4});
+    v.emplace_back({1, 4});
+
+    vector<int> v(5, 100);
+
+    vector<int> v(5);
+
+    vector<int> v1(6, 90);
+    vector<int> v2(v1);
 }
 
 int main(){
